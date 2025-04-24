5009f311185da3b22d72a7f773d30a1f9c59c4dd
Julian Repls <juliujuliusamsung@gmail.com>
2025-04-24 13:39:44 +0200

    Agrego la función de recibir libro, creo un estilo mejorado de html

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse #Used to generate URLs by reversing the URL patt
 import uuid # Requerida para las instancias de libros únicos
 from django.contrib.auth.models import User
 from datetime import date
+from django.utils.html import mark_safe
 
 # Create your models here.
 
@@ -34,7 +35,10 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text="Ingrese una breve descripción del libro")
 
 
-    isbn = models.CharField('ISBN',max_length=13, help_text='13 Caracteres <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    isbn = models.CharField('ISBN',
+                            max_length=13,
+                            help_text=mark_safe('13 Caracteres <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+                            )
 
     genre = models.ManyToManyField(Genre, help_text="Seleccione un genero para este libro")
     # ManyToManyField, porque un género puede contener muchos libros y un libro puede cubrir varios géneros.
